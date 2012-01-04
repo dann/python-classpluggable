8a719344de55d2e6ab5d741dc31bea2920defc42
Dann <techmemo@gmail.com>
2012-01-04 16:18:54 +0900

    refactored

--- classpluggable/__init__.py
+++ classpluggable/__init__.py
@@ -23,20 +23,28 @@ class PluginLoader(object):
 
     def __init__(self):
         self.plugins = []
+        self.suffix = '.py'
 
     def load_plugins(self, plugin_dir):
+        if len(self.plugins) > 0:
+            return self.plugins
+
         plugins = self._find_plugins(plugin_dir)
+        self._add_path_to_plugins(plugin_dir)
         self._import_plugins(plugin_dir, plugins)
         self._register_plugins()
         return self.plugins
 
     def _find_plugins(self, plugin_dir):
         plugin_files = [f[:-3] for f in os.listdir(plugin_dir)
-                        if f.endswith('.py') and not f.startswith('_')]
+                        if f.endswith(self.suffix)
+                        and not f.startswith('_')]
         return plugin_files
 
-    def _import_plugins(self, plugin_dir, plugin_files):
+    def _add_path_to_plugins(self, plugin_dir):
         sys.path.insert(0, plugin_dir)
+
+    def _import_plugins(self, plugin_dir, plugin_files):
         for plugin in plugin_files:
             try:
                 __import__(plugin)
@@ -46,5 +54,9 @@ class PluginLoader(object):
     def _register_plugins(self):
         for plugin in Plugin.__subclasses__():
             self.plugins.append(plugin())
+
+    def reload_plugins(self, plugin_dir):
+        self.plugins = []
+        return self.load_plugins(plugin_dir)
 
 
